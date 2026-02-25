0fa4dc96db95d24074469839f15ecb7ae308c9ee
ASHURA-deb <folajimigandonu@gmail.com>
2026-02-25 01:50:42 +0100

    removed commands and added new commands

--- deb-install/tools/system.sh
+++ deb-install/tools/system.sh
@@ -14,6 +14,20 @@ echo "Installing btop"
 sudo add-apt-repository ppa:bashtop-monitor/bashtop && sudo apt update && sudo apt install bashtop
 echo "Done"
 
+echo "Installing Syncthing"
+# Add the release PGP keys:
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -L -o /etc/apt/keyrings/syncthing-archive-keyring.gpg https://syncthing.net/release-key.gpg
+
+# Add the "stable-v2" channel to your APT sources:
+echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing stable-v2" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
+# Add the "candidate" channel to your APT sources:
+echo "deb [signed-by=/etc/apt/keyrings/syncthing-archive-keyring.gpg] https://apt.syncthing.net/ syncthing candidate" | sudo tee /etc/apt/sources.list.d/syncthing.list
+
+# Update and install syncthing:
+sudo apt-get update
+sudo apt-get install syncthing
 
 echo "Installing timeshift"
 sudo apt install timeshift -y
